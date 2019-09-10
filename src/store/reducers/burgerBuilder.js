52672a212b15ec2cf38a1e718463286e26d10aa0
jdbbaugh <jbaugh78@msn.com>
2019-09-10 07:40:25 -0500

    saved

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from "../utility";
 const initialState = {
   ingredients: null,
   totalPrice: 4,
-  error: false
+  error: false,
+  building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -21,14 +22,16 @@ const addIngredient = (state, action) => {
   const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
   const updatedState = {
     ingredients: updatedIngredients,
-    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+    building: true
   };
   return updateObject(state, updatedState);
 };
 
 const removeIngredient = (state, action) => {
   const updatedIng = {
-    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+    [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+    building: true
   };
   const updatedIngs = updateObject(state.ingredients, updatedIng);
   const updatedSt = {
@@ -47,7 +50,8 @@ const setIngredients = (state, action) => {
       meat: action.ingredients.meat
     },
     totalPrice: 4,
-    error: false
+    error: false,
+    building: false
   });
 };
 
